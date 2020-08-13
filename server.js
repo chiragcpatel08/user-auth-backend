a60c9dea57093ef11ff376d9514a4569d65bd525
chiragcpatel170884 <67153685+chiragcpatel170884@users.noreply.github.com>
2020-08-13 03:32:28 -0400

    add database

--- server.js
+++ server.js
@@ -1,80 +1,80 @@
+require('dotenv').config();
 const express = require('express');
-const { json } = require('body-parser');
+const cors = require('cors');
 const app = express();
+const knex = require('knex')
+const bcrypt = require("bcrypt-nodejs");
 
 app.use(express.json());
+app.use(cors());
 
-const users = [{
-    id: "1",
-    name: "chirag",
-    image: "",
-    email: "chirag@gmail.com",
-    password: "chirag",
-    joined: new Date()
-}, {
-    id: "2",
-    name: "vaishu",
-    image: "",
-    email: "vaishu@gmail.com",
-    password: "vaishu",
-    joined: new Date()
-}]
+const database = knex({
+    client: 'pg',
+    connection: {
+    host : process.env.DB_HOST,
+    user : process.env.DB_USER,
+    password : process.env.DB_PASSWORD,
+    database : process.env.DB_DATABASE
+    }
+});
 
 app.get("/", (req, res) => {
-    res.json(users)
+    database.select("*").from("users")
+    .then(users => res.json(users))
 })
 
-app.post("/login", (req, res) =>{
-    const filteredUser = users.filter((user) => {
-        return (req.body.email === user.email && req.body.password === user.password)
+app.post("/login", (req, res) => {
+    const {email, password} = req.body;
+    database.select('email', 'hash').from('login').where({email: email})
+    .then(data => {
+        const isValid = bcrypt.compareSync(password, data[0].hash);
+        if(isValid){
+            database.select('*').from('users').where({email: email})
+            .then(user=>{
+                res.json(user[0]);
+            })
+            .catch(err=> {
+                res.status(400).json("Spurious User Credentials");
+            })            
+        } else {
+            res.status(400).json("Invalid User Credentials");
+        }
     })
-    if(filteredUser.length) {
-        res.json(filteredUser[0])
-    } else {
-        res.status(404).json("Not a valid user");
-    }
-})
+    .catch(err=> {
+        res.status(400).json("Wrong User Credentials");
+    })
+});
 
 app.post("/register", (req, res) => {
-    const {id, name, email, password, image} = req.body;
-    const newUser = {
-        id: id,
-        name: name,
-        image: image,
-        email: email,
-        password: password,
-        joined: new Date()
-    }
-    if (newUser.email) {
-        users.push(newUser);
-        res.json(newUser);
-    } else {
-        res.status(404).json("Something went wrong");
-    }    
-})
-
-app.get("/:id", (req, res) => {
-    const filteredUser = users.filter((user) => {
-        return (req.params.id === user.id)
+    const {name, email, image, password} = req.body;
+    const hash = bcrypt.hashSync(password);
+    database.transaction(trx => {    
+        trx.insert({
+            hash: hash,
+            email: email
+        })
+          .into('login')
+          .returning('email')
+          .then(loginEmail => {
+            return trx('users')
+            .returning('*')
+            .insert({
+                name: name,
+                email: loginEmail[0],
+                image: image,
+                joined: new Date()
+            })
+            .then(newUser => {
+                res.json(newUser[0]);
+            })            
+          })
+          .then(trx.commit)
+          .catch(trx.rollback);
+      })
+      .catch(err => {
+        res.status(400).json("Unable to register user");
     })
-    if(filteredUser.length) {
-        res.json(filteredUser[0])
-    } else {
-        res.status(404).json("User not found");
-    }
-})
-
-app.put("/:id", (req, res) => {
-    const matchedUser = users.filter((user) => {
-        return (req.params.id === user.id)
-    })
-    if(matchedUser.length) {
-        matchedUser[0].image = req.body.image;       
-        res.json(matchedUser[0]);
-    } else {
-        res.status(404).json("User not found");
-    }
-})
+});
 
 app.listen(3001, () => {
     console.log("server has started");
